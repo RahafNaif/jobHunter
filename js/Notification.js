c9b551da15e681f19b414d4a47e9d0e1c11cce9e
ha4lool <dalalbinhumaid@gmail.com>
2021-04-03 03:04:43 +0300

    notification js

--- js/Notification.js
+++ js/Notification.js
@@ -1,11 +1,12 @@
-// Popup Open
-function popupOpen(){
-    document.getElementById("popup").style.display="block";
-    document.getElementById("overlay").style.display="block";
-	}
-    
-// Popup Close
-	function popupClose(){
-    document.getElementById("popup").style.display="none";
-    document.getElementById("overlay").style.display="none";
-	}
+$(document).ready(function(){
+
+  });
+
+  function showNotifications(){
+    document.getElementById("popup-inner").style.display = "block";
+  }
+
+
+  function hideNotification(){
+    document.getElementById("popup-inner").style.display = "none";
+  }

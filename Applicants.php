c32eae9da19c9a6c589f6f7779304ace89a84e5b
shahad0210 <51061154+shahad0210@users.noreply.github.com>
2021-04-07 22:42:35 +0300

    .

--- Applicants.php
+++ Applicants.php
@@ -90,7 +90,7 @@ if (!isset($_SESSION['email'])) {
           <a href="#"><i class="material-icons">person</i></a>
           <ul>
             <li><a href="EmployerProfile.html">Profile</a></li>
-            <li><a href="home.html">Log out</a></li>
+            <a href="http:signout.php">logout</a>
           </ul>
         </li>
       </ul>

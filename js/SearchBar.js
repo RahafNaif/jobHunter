be8845583e32880bbf50093a38ee021645ef5ec6
RahafNaif <49600567+RahafNaif@users.noreply.github.com>
2021-04-10 01:15:14 +0300

    update search bar

---
+++ js/SearchBar.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    $('#position-option').click(function () { 
+        $('#selected').html('position');
+    });
+
+    $('#major-option').click(function () { 
+        $('#selected').html('major');
+    });
+
+    $('#name-option').click(function () { 
+        $('#selected').html('company name');
+    });
+
+    $('#citybtn').click(function () { 
+       var city =  $('#city').val();
+       $('#city-select').html(city);
+    });
+
+    $('#full').click(function () { 
+        $('#time').html('Full time');
+    });
+
+    $('#part').click(function () { 
+        $('#time').html('Part time');
+    });
+
+});

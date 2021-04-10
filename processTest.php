17a4f02761f70325844d711bcc96e6461ba5f15c
faymk <51158047+faymk@users.noreply.github.com>
2021-04-10 12:36:46 +0300

    update

--- processTest.php
+++ processTest.php
@@ -3,14 +3,23 @@
 $dbServername= "localhost";
 $dbUsername="root";
 $dbPassword="";
-$dbname="test";
+$dbname="jobhunter";
 
 $conn= mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbname) or  die("cannot connect".mysqli_connect_errno());
 
 if (isset($_POST['save'])){
+$firstName=$_POST['firstName'];
+$lastName=$_POST['lastName'];
 $email=$_POST['email'];
 $password=$_POST['password'];
-$result= $conn->query("INSERT INTO test(email, password )VALUES('$email','$password')") or 
+$birthDate=$_POST['birthDate'];
+$gender=$_POST['gender'];
+$nationality=$_POST['nationality'];
+$city=$_POST['city'];
+$phone=$_POST['phone'];
+$major=$_POST['major'];
+$currentJob=$_POST['currentJob'];
+$result= $conn->query("INSERT INTO jobseeker(firstName, lastName,email,password, birthDate, gender,  nationality , city , phone,major, currentJob )VALUES('$firstName','$lastName','$email','$password','$birthDate','$gender','$nationality','$city','$phone','$major','$currentJob')") or 
 die($conn->error);
 $res=mysqli_query($conn,$result);
 if($res)

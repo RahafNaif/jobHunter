8c4b2ecf6f7e51e49125182a88f41d1b7822c639
faymk <51158047+faymk@users.noreply.github.com>
2021-04-13 21:43:45 +0300

    Add social media fileds

--- JobSeekerViewProf.php
+++ JobSeekerViewProf.php
@@ -187,24 +187,24 @@ echo $result ;
  $result = mysqli_query($database, $query);
  if ($result) {
      while ($data = mysqli_fetch_assoc($result)) {
-        $firstName=$data['firstName'];
-        $lastName=$data['lastName'];
-        $email=$data ['email'];
-        $password=$data['password'];
-        $bDate=$data['birthDate'];
+        $firstName = $data['firstName'];
+        $lastName = $data['lastName'];
+        $email = $data['email'];
+        $password = $data['password'];
+        $bDate = $data['birthDate'];
         $birthDate = date("d-m-Y", strtotime($bDate));
-        $gender=$data['gender'];
-        $nationality=$data['nationality'];
-        $city=$data['city'];
-        $phone=$data['phone'];
-        $major=$data['major'];
-        $experince=$data['experince'];
-        $skills=$data['skills'];
-        $currentJob=$data['currentJob'];
-        $Website=$date['Website'];
-        $Github=$date['Github'];
-        $Instagram=$date['Instagram'];
-        $Facebook=$date['Facebook'];
+        $gender = $data['gender'];
+        $nationality = $data['nationality'];
+        $city = $data['city'];
+        $phone = $data['phone'];
+        $major = $data['major'];
+        $experince = $data['experince'];
+        $skills = $data['skills'];
+        $currentJob = $data['currentJob'];
+        $Website = $data['Website'];
+        $Github = $data['Github'];
+        $Instagram = $data['Instagram'];
+        $Facebook = $data['Facebook'];
      }
  } else {
      echo "There are no info.";
@@ -242,25 +242,37 @@ echo $result ;
                 </div>
             </div>
             
-            <!-- <div class="details1">
+    <div class="details1">
                 
                   
                       <li class="group-list-item display-flex justify-between align-center  flex-wrap">
-                        <h6 class="margin-bottom-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe margin-right-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
-                        <p class="gray-color" name="Website"><?php echo $Website?></p>
+        <h6 class="margin-bottom-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe margin-right-2 icon-inline">
+            <circle cx="12" cy="12" r="10"></circle>
+            <line x1="2" y1="12" x2="22" y2="12"></line>
+            <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+          </svg>Website</h6>
+        <p class="gray-color" name="Website"><?php echo $Website ?></p>
                       </li> 
                        <li class="group-list-item display-flex justify-between align-center  flex-wrap">
-                        <h6 class="margin-bottom-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github margin-right-2 icon-inline"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>Github</h6>
-                        <p class="gray-color" name="Github"><?php echo $Github?></p>
+        <h6 class="margin-bottom-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github margin-right-2 icon-inline">
+            <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+          </svg>Github</h6>
+        <p class="gray-color" name="Github"><?php echo $Github ?></p>
                     
                     
                       <li class="group-list-item display-flex justify-between align-center  flex-wrap">
-                        <h6 class="margin-bottom-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram margin-right-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>Instagram</h6>
-                        <span class="gray-color"name="Instagram"><?php echo $Instagram?></span>
+        <h6 class="margin-bottom-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-instagram margin-right-2 icon-inline text-danger">
+            <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+            <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+          </svg>Instagram</h6>
+        <span class="gray-color" name="Instagram"><?php echo $Instagram ?></span>
                       </li> 
                        <li class="group-list-item display-flex justify-between align-center  flex-wrap"> 
-                        <h6 class="margin-bottom-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook margin-right-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>Facebook</h6>
-                        <p class="gray-color" name= "Facebook"><?php echo $Facebook?></p>
+        <h6 class="margin-bottom-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-facebook margin-right-2 icon-inline text-primary">
+            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+          </svg>Facebook</h6>
+        <p class="gray-color" name="Facebook"><?php echo $Facebook ?></p>
                        </li> 
                 
                   </div> -->

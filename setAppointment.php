9faf50a0b1ee0580affc37a7050c20ec618d77dc
rand-02 <77927596+rand-02@users.noreply.github.com>
2021-04-15 06:29:26 +0300

    .

--- setAppointment.php
+++ setAppointment.php
@@ -251,7 +251,7 @@
     </div>
     <div class="addForm-parent">
       <div class="addForm">
-        <a href="setAppointment.html" class="closeIcon"
+        <a href="setAppointment.php" class="closeIcon"
           ><svg
             xmlns="http://www.w3.org/2000/svg"
             enable-background="new 0 0 24 24"
